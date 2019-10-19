1931167e81922ce8f5b9ec8e8af8d408fd9746de
= <=>
2019-10-19 14:26:34 -0300

    get usuario by id

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.listen(app.get('port'), () => {
             
             app.get('/usuario/:id', (req,res) => {
               const id = req.params.id;
-              pool.query('SELECT * FROM usuarios WHERE id=$1', [id]);
+             const response = pool.query('SELECT * FROM usuarios WHERE id=$1', [id]);
               res.json(response.rows);
             });
 
