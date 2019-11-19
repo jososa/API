822853c157a2db03547a575d0fa8c5cbb3dce615
= <=>
2019-11-19 00:12:31 -0300

    add user pass

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ app.listen(app.get('port'), () => {
                       message: 'Email ya existente',
                       });
                   }
-                  console.log(values);
+                  console.log(values[0,1,2,3,4,5,6,7,8]);
                   res.status(200).send({
                     status: 'OK',
                     result: result.rows[0],
